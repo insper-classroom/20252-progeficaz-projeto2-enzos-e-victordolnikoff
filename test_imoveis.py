d4c99b1ff2e1ecb9d12b79f43627c50b8fcae04d
Victor Dolnikoff <vividolni@gmail.com>
2025-09-02 09:06:48 -0300

    inicio testes

---
+++ test_imoveis.py
@@ -0,0 +1,26 @@
+import pytest
+import json
+
+
+def test_listar_imoveis():
+    return
+
+
+def test_listar_imoveis_id():
+    return
+
+
+def test_novo_imovel():
+    return
+
+
+def test_del_imovel():
+    return
+
+
+def test_listar_imoveis_tipo():
+    return
+
+
+def test_listar_imoveis_cidade():
+    return
